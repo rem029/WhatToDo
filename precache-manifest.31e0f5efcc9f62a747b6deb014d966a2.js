42e25d1e74e03c6298cdbeb09d9ccbae7edc34f0
Lawrence Ponce <elawrenceponce@gmail.com>
2020-07-19 20:25:18 +0300

    Updates

---
+++ precache-manifest.31e0f5efcc9f62a747b6deb014d966a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680a426e85b4ca0eb972152a5052969b",
+    "url": "/WhatToDo/index.html"
+  },
+  {
+    "revision": "6b48297224eed8d5cd5a",
+    "url": "/WhatToDo/static/css/2.02b1f3e9.chunk.css"
+  },
+  {
+    "revision": "d4035c0891cca4f951d9",
+    "url": "/WhatToDo/static/css/main.46c79476.chunk.css"
+  },
+  {
+    "revision": "6b48297224eed8d5cd5a",
+    "url": "/WhatToDo/static/js/2.6845d3a6.chunk.js"
+  },
+  {
+    "revision": "d4035c0891cca4f951d9",
+    "url": "/WhatToDo/static/js/main.61a1729f.chunk.js"
+  },
+  {
+    "revision": "52b402ebb1f6ce423aca",
+    "url": "/WhatToDo/static/js/runtime~main.a6ddf33b.js"
+  }
+]);
