ae89ddddc58228171b942189a16db215fe174d96
Lawrence Ponce <elawrenceponce@gmail.com>
2020-07-25 15:55:10 +0300

    updated header. added made by rem029

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,5 +4,10 @@ import "../assests/css/fonts/fonts.css";
 import "./Header.css";
 
 export default function Header(props) {
-  return <div className="header">{props.title}</div>;
+	return (
+		<div className="header">
+			<h1>{props.title}</h1>
+			<p>Made by rem029</p>
+		</div>
+	);
 }
